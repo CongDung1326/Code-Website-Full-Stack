bfe2b61f157a714231bfe955d75aaa2784aef9ea
CongDung1326 <dung451008@gmail.com>
2023-08-10 12:52:32 +0700

    Update SQL

--- NodeJs/src/services/userAPIsService.js
+++ NodeJs/src/services/userAPIsService.js
@@ -117,8 +117,10 @@ let createNewUser = (data) => {
                     lastName: data.lastName,
                     address: data.address,
                     phoneNumber: data.phoneNumber,
-                    gender: data.gender === '1' ? true : false,
+                    gender: data.gender,
+                    image: data.image,
                     roleId: data.roleId,
+                    positionId: data.positionId,
                 });
 
                 resolve({
