38f16d2f07489070ba9311914a73d1d3716097b0
CongDung1326 <dung451008@gmail.com>
2023-08-23 16:56:35 +0700

    Create table doctor_id and add price,payment,province for table allcodes

---
+++ NodeJs/src/migrations/migration-create-doctor-id.js
@@ -0,0 +1,58 @@
+'use strict';
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        // Muốn add table thì đọc thêm ở query sequelize
+        await queryInterface.createTable('doctor_id', {
+            // Ở user.js khai báo gì thì ở đây khái báo tương tự vậy
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            docterId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+            },
+            priceId: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            provinceId: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            paymentId: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            addressClinic: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            nameClinic: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            note: {
+                type: Sequelize.STRING,
+            },
+            count: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                defaultValue: 0,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('doctor_id');
+    }
+};
